78885fed2589aeee9fb457e231fccecd3d47b9ed
22708809 <D111365@YAHOO.COM.TW>
2016-11-10 17:51:17 +0800

    Add files via upload

--- web3/js/common.js
+++ web3/js/common.js
@@ -1,3 +1,4 @@
+  /* MENU */
 $(function(){
 		/*prop.utilitiesContainer_switch.on('click', function(){
 			// ナビゲーションと、ヘッダユーティリティ領域の表示／非表示を切り替える
@@ -7,4 +8,42 @@ $(function(){
 	$('.ulicon').click(function(){
 		$('#nav ul').slideToggle();
 	});
+});
+
+
+  /* 上下滾動 */
+$(function(){
+  /* home_line plugin */
+  var $Ul = $('.home_line ul'),
+      $LI = $Ul.append($Ul.html()).children(),
+      _height = $('.home_line').height() * -1,
+      scrollSpeed = 900,
+      timer,
+      speed = 10 + scrollSpeed;
+  
+  $LI.hover(function(){ clearTimeout(timer); }, function(){  timer = setTimeout(showad, speed);	});
+ 
+	function showad(){
+		var _now = $Ul.position().top / _height;
+		_now = (_now + 1) % $LI.length;
+ 
+		$Ul.animate({
+			top: _now * _height
+		}, scrollSpeed, function(){
+			if(_now == $LI.length / 2){
+				$Ul.css('top', 0);
+			}
+		});
+		timer = setTimeout(showad, speed);
+	}
+ 
+	timer = setTimeout(showad, speed);
+ 
+	$('.home_line a').focus(function(){
+		this.blur();
+	});
+  
+  $(".panel_menu button").click(function(){
+    $(".panel_menu ul").toggleClass("open");
+  });
 });
